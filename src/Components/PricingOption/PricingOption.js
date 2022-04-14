9baaf19a00ba8798ba59d69ba64c433f464f58ee
nurulislamrimon <nurulislamrimon@gmail.com>
2022-04-14 15:18:22 +0600

    done

--- src/Components/PricingOption/PricingOption.js
+++ src/Components/PricingOption/PricingOption.js
@@ -5,7 +5,7 @@ import Feature from '../Features/Feature';
 const PricingOption = (props) => {
     const { name, price, features } = props.option;
     return (
-        <div className='bg-white p-1'>
+        <div className='bg-white p-1 pb-12 relative'>
             <h1 className='bg-red-300 rounded-lg p-4 font-bold text-xl'>
                 {name}
             </h1>
@@ -15,6 +15,8 @@ const PricingOption = (props) => {
 
             <h2 className='text-2xl text-left'>Features:</h2>
             {features.map(feature => <Feature feature={feature}></Feature>)}
+
+            <button className='bg-red-300 p-2 absolute bottom-1 left-0 rounded-3xl w-full hover:text-white hover:bg-red-500'>Submit</button>
         </div>
     );
 };
